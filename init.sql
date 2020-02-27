c2e03aa170f7004fca712126edf240c8e914e69c
myunusov <myunusov@luxoft.type>
2020-02-27 15:26:09 +0300

    Add gw kong and Auth with keycloak

--- init.sql
+++ init.sql
@@ -1,4 +1,13 @@
 create database users;
+
 create database sentry;
 create user sentry with password 'sentry';
 grant all privileges on database sentry to sentry;
+
+create database kong;
+create user kong with password 'kong';
+grant all privileges on database kong to kong;
+
+create database keycloak;
+create user keycloak with password 'keycloak';
+grant all privileges on database keycloak to keycloak;
